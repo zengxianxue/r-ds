10da014155b119056f3cd124816e7a8c44a9fc12
zengxianxue <zengxianxue@gmail.com>
2022-04-26 11:04:31 +0800

    init project

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+mod hash_table;
+mod heap;
+mod stack;
+mod queue;
+mod array;
+mod linked_list;
+mod tree;
+mod graph;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
